dd3bcf4516c3fbd2e3d08655c06c225af4ffbc1e
Ryan <kimjhs@kakao.com>
2023-08-08 17:52:23 +0900

    수정 아이콘 교체 및 오타 수정

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write --ignore-unknown'],
-  '**/*.ts?(x)': () => 'npm run build:types',
   '*.json': ['prettier --write --ignore-unknown']
 };
